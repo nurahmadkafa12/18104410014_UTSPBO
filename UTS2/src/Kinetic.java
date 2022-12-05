f94d2cd1132b0ead9090d3106fd5c08db98d236b
nurahmadkafa12 <119824909+nurahmadkafa12@users.noreply.github.com>
2022-12-05 12:29:09 +0700

    Add files via upload

---
+++ UTS2/src/Kinetic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author AZIFA
+ */
+public class Kinetic {
+    int Ek, m, v;
+    Kinetic(){
+        Ek = 1/2(m*v)
+        
+    }
+}
---
+++ UTS2/src/MainEnergy.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author AZIFA
+ */
+public class MainEnergy extends Kinetic{
+    
+    public static void main(String[] args) {
+        MainEnergy mainEnergy = new Kinetic();
+        
+        Kinetic.
+        
+    }
+    
+}
